8453d48e5c90c1c9ae05ba7d7eaf5e3c8dcab6c4
nabil-y <nabil.yassine.dev@gmail.com>
2022-05-31 22:54:18 +0200

    Refactor dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,19 +1,36 @@
 import { ReactElement } from "react";
 import NutritionInfo from "./NutritionInfo/NutritionInfo";
 import BarChart from "./Charts/BarChart";
-import BottomCharts from "./BottomCharts";
 import styles from "./Dashboard.module.css";
+import Card from "../UI/Card";
+import LineChart from "./Charts/LineChart";
+import SpiderChart from "./Charts/SpiderChart";
+import RadialChart from "./Charts/RadialChart";
 
 /**
  * Dashboard Function
  * @returns Dashboard Component
  */
 const Dashboard = (): ReactElement => {
+  const bottomCharts: [ReactElement, string][] = [
+    [<LineChart />, "line-chart"],
+    [<SpiderChart />, "spider-chart"],
+    [<RadialChart />, "radial-chart"],
+  ];
   return (
     <div className={styles.dashboard}>
       <div className={styles["dashboard-left"]}>
         <BarChart />
-        <BottomCharts />
+        <div className={styles["bottom-charts"]}>
+          {bottomCharts.map((item) => (
+            <Card
+              key={item[1]}
+              className={`${styles["bottom-chart"]} ${styles[item[1]]}`}
+            >
+              {item[0]}
+            </Card>
+          ))}
+        </div>
       </div>
       <NutritionInfo />
     </div>
